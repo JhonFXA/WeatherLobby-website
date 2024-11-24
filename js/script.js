458537d96a90b962002327394f54805e87155e2b
JhonFXA <jonathanbispo085@gmail.com>
2024-11-24 16:15:19 -0300

    mudar tema do site implementado

--- js/script.js
+++ js/script.js
@@ -20,25 +20,59 @@ function headerShadow(){
     }
 }
 
-function switchTheme(){ 
+function switchTheme(){
+
+    const firstIllustration = document.getElementById("first-illustration");
+    const secondIllustration = document.getElementById("second-illustration");
+    const themeIcon = document.getElementById("theme-icon");
 
     if(currentTheme == "light"){
-        page.style.setProperty("--background-gradient1-color", getComputedStyle(page).getPropertyValue("--background-darktheme-gradient1-color")); 
-        page.style.setProperty("--background-gradient2-color", getComputedStyle(page).getPropertyValue("--background-darktheme-gradient2-color")); 
+        changeColorAttribute("--background-gradient1-color", "--background-darktheme-gradient1-color");
+        changeColorAttribute("--background-gradient2-color", "--background-darktheme-gradient2-color");
+
         if(page.scrollTop > 50){
             header.style.backgroundColor = getComputedStyle(page).getPropertyValue("--background-darktheme-gradient2-color");
             header.style.borderBottom = `solid 1px ${getComputedStyle(page).getPropertyValue("--border-darktheme-color")}`;
         }
+
+        changeColorAttribute("--primary-text-color", "--white-text-color");
+        changeColorAttribute("--secondary-text-color", "--real-black-text-color");
+        changeColorAttribute("--result-box-background", "--result-box-darktheme-background");
+        changeColorAttribute("--footer-background", "--footer-darktheme-background");
+        changeColorAttribute("--border-color", "--border-darktheme-color");
+        changeColorAttribute("--switch-theme-background", "--switch-theme-darktheme-background");
+        changeColorAttribute("--switch-theme-color", "--switch-theme-darktheme-color");
+
+        firstIllustration.setAttribute("src", "../img/moonrocket-illustration.png");
+        secondIllustration.setAttribute("src", "../img/telescope-illustration.png");
+        themeIcon.setAttribute("class", "fa-solid fa-sun");
+        
         currentTheme = "dark"; 
     } else if(currentTheme == "dark"){ 
-        page.style.setProperty("--background-gradient1-color", getComputedStyle(page).getPropertyValue("--background-lighttheme-gradient1-color")); 
-        page.style.setProperty("--background-gradient2-color", getComputedStyle(page).getPropertyValue("--background-lighttheme-gradient2-color"));
+        changeColorAttribute("--background-gradient1-color", "--background-lighttheme-gradient1-color");
+        changeColorAttribute("--background-gradient2-color", "--background-lighttheme-gradient2-color");
+        
         if(page.scrollTop > 50){
             header.style.backgroundColor = getComputedStyle(page).getPropertyValue("--background-lighttheme-gradient2-color");
             header.style.borderBottom = `solid 1px ${getComputedStyle(page).getPropertyValue("--border-lighttheme-color")}`;
         }
+        changeColorAttribute("--primary-text-color", "--black-text-color");
+        changeColorAttribute("--result-box-background", "--result-box-lighttheme-background");
+        changeColorAttribute("--footer-background", "--footer-lighttheme-background");
+        changeColorAttribute("--border-color", "--border-lightheme-color");
+        changeColorAttribute("--switch-theme-background", "--switch-theme-lighttheme-background");
+        changeColorAttribute("--switch-theme-color", "--switch-theme-lighttheme-color");
+        
+        firstIllustration.setAttribute("src", "../img/ballon-illustration.png");
+        secondIllustration.setAttribute("src", "../img/beach-illustration.png");
+        themeIcon.setAttribute("class", "fa-solid fa-moon");
+
         currentTheme = "light";
     }
+}
+
+function changeColorAttribute(property, value){
+    page.style.setProperty(property, getComputedStyle(page).getPropertyValue(value));
 }
 
 window.onscroll = function(){
